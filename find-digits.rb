1080e6f5a6291b9332700288c0f53e6dea952cd6
Matthew Villwock <mtvillwock@gmail.com>
2015-06-04 11:56:14 -0700

    pass first test case but fails others

--- find-digits.rb
+++ find-digits.rb
@@ -9,34 +9,27 @@
 # - find digits in number that exactly divide N (leaving 0)
 # - count same number twice if repeated
 # - dividing by 0 is undefined
+# - build array of valid digits to test
 # - starting with counter at 1, check reminder of input divided by counter
 # - increment output for each time the remainder of input % counter == 0
 
-cases = gets().rstrip.to_i
-count = 0
-iteration = 0
-collection = []
-cases.times do
-  input = gets().rstrip.to_i
-  p "input is: #{input}"
-  until iteration == input
-    iteration += 1
-      p "iteration is: #{iteration}"
-    remainder = input % iteration
-      p "remainder is: #{remainder}"
-    if remainder == 0
-      collection << iteration
+def find_digits(number)
+  digits = number.to_s.chars.map { |el| el.to_i }
+  count = 0
+  digits.each do |digit|
+    if digit == 0
+      next
+    else
       count +=1
-      p "count is: #{count}"
     end
   end
-  p "collection pre-uniq method"
-  p collection
-  collection.uniq!
-  p "count of time remainder from modulo == 0"
-  puts count
-  p "collection count"
-  puts collection.count
+  p count
+end
+
+cases = gets().rstrip.to_i
+cases.times do
+  number = gets().rstrip
+  find_digits(number)
 end
 
 # Example Input (number of test cases, followed by individual cases)
