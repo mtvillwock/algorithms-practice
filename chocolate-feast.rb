27fd58200ca972929a1d8a8923a586eb1bed17eb
Matthew Villwock <mtvillwock@gmail.com>
2015-06-08 10:22:58 -0700

    pass all test cases, revise pseudocode

--- chocolate-feast.rb
+++ chocolate-feast.rb
@@ -11,8 +11,12 @@
 
 # Pseudocode
 # divide money by price of chocolate to get number of candy bars
-# if number of candy bars is a multiple of M, add that multiple to output
-#
+# have wrappers equal to chocolates bought and compare with exchange rate
+# while wrappers plus bonus chocolates are higher or equal to the exchange rate
+# take the divmod of wrappers plus bonus chocolates
+# -set bonus chocolates equal to the quotient and wrappers equal to the remainder
+# add bonus chocolates to the total chocolates
+# print the total
 
 def count_chocolates
   (dollars, cost, exchange_rate) = gets().split.map{|i| i.to_i}
@@ -20,8 +24,9 @@ def count_chocolates
   bonus_chocolates = 0
   wrappers = total_chocolates_eaten
   while (wrappers + bonus_chocolates) >= exchange_rate do
-      bonus_chocolates = (bonus_chocolates + wrappers) / exchange_rate
-      wrappers = wrappers / exchange_rate
+      # bonus_chocolates is the quotient of this equation
+      # wrappers are the remainder, e.g., 3 /2 leaves 1 wrapper
+      bonus_chocolates, wrappers = (wrappers + bonus_chocolates).divmod(exchange_rate)
       total_chocolates_eaten += bonus_chocolates
     end
     puts total_chocolates_eaten
@@ -32,8 +37,8 @@ def count_chocolates
     count_chocolates
   end
 
-# Test input
-# 3
-# 10 2 5
-# 12 4 4
-# 6 2 2
+  # Test input
+  # 3
+  # 10 2 5
+  # 12 4 4
+  # 6 2 2
