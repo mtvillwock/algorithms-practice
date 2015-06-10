10458d56f97a27b300cfcdeb484cb09b15ef838b
Matthew Villwock <mtvillwock@gmail.com>
2015-06-10 10:08:26 -0700

    passing first 2 test cases; going to try others

--- sherlock-array.rb
+++ sherlock-array.rb
@@ -19,27 +19,36 @@
 # YES
 
 # psuedocode
-# if number of elements is less than 3, return NO
-# else, for each element, check if it has elements to the left and right
-# sum these elements and compare, returning YES or NO depending on if they are equal sums
+# for each element, sum the range of elements to its left and right
+# return YES or NO depending on if left range and right range have equal sums
 
 cases = gets().rstrip.to_i
 def sum_left_equal_sum_right?
   num_of_elements = gets().rstrip
   numbers = gets().rstrip.split(" ")
-  p numbers_array = numbers.map { |e| e.to_i  }
+  numbers_array = numbers.map { |e| e.to_i  }
   left_sum = 0
   right_sum = 0
-  numbers_array.each_with_index do |number, index|
+  numbers_array.each_with_index do |num, index|
+    p "index is: #{index}"
+    p "current num is: #{num}"
     if index == 0
       next
-    elsif numbers_array[index+1] == numbers_array[-1]
+    elsif index == numbers_array.length - 1
       break
+    elsif index == 1
+      left_sum = numbers_array[0]
+      right_sum = (numbers_array[index + 1]..numbers_array[-1]).reduce(:+)
     else
-    p left_array = (numbers_array[0]..numbers_array[index -1]).to_a
-    p right_array = (numbers_array[index + 1]..numbers_array[-1]).to_a
+      left_range = (numbers_array[0]...numbers_array[index])
+      right_range = (numbers_array[index + 1]..numbers_array[-1])
+      p "left_range is #{left_range}"
+      p "right_range is #{right_range}"
+      left_sum = left_range.reduce(:+)
+      right_sum = right_range.reduce(:+)
     end
   end
+  p "post-loop, right sum is #{right_sum} and left sum is #{left_sum}"
   if left_sum == right_sum
     puts "YES"
   else
