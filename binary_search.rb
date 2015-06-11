3c0cfc32107e14635d138551e7d49e94c6207030
Matthew Villwock <mtvillwock@gmail.com>
2015-06-10 22:35:43 -0700

    WIP - problems with inputs higher than mid

--- binary_search.rb
+++ binary_search.rb
@@ -8,18 +8,19 @@
 # find the low, mid, and high index of the given array
 # if the target is equal to the index of the mid array, return true
 # if the target is greater/less than the value of index of the high/low array, return false
-# while the
-#
-
+# while the target is not the array value of the mid index
+# if the target is greater than value of the array's mid index
+# => set the low index to the mid index
+# => set the mid index to the sum of the high index minus the low index divided by two
+# elsif
+# => set the high index to the mid index
+# => set the mid index to the sum of the high index minus the low index divided by two
+# return true if the value of the array's mid index equals the target
 
 def binary_search_iterative(target, array)
   low_index = 0
-  mid_index = (array.length / 2)
-  high_index = (array.length - 1)
-  # handle instant find input
-  if target == array[mid_index]
-    return "index of target is #{mid_index}"
-  end
+  high_index = array.length - 1
+
   # handle out of bounds inputs
   if target > array[high_index]
     return false
@@ -27,13 +28,19 @@ def binary_search_iterative(target, array)
     return false
   end
   # start loop of binary chop operations
-  while target != array[mid_index] do
-      p "target is #{target}"
-      p "low is #{low_index}, mid is #{mid_index}, high is #{high_index}"
-      if target > array[mid_index]
+  while low_index <= high_index do
+      mid_index = (high_index + low_index) / 2
+
+      # handle instant find input
+      if target == array[mid_index]
+        return "index of target is #{mid_index}"
+      elsif target > array[mid_index]
+        sleep(2.0)
+        p "target is #{target}"
+        p "low is #{low_index}, mid is #{mid_index}, high is #{high_index}"
         low_index = mid_index
         p "new low is #{low_index}"
-        mid_index = (high_index - low_index) / 2
+        mid_index = mid_index + ((high_index - low_index) / 2)
         p "new mid is #{mid_index}"
       elsif target < array[mid_index]
         high_index = mid_index
@@ -45,10 +52,27 @@ def binary_search_iterative(target, array)
     return true if target == array[mid_index]
   end
 
+  # search for valid target below mid
   p binary_search_iterative(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
   #=> expect to return index 2
+
+  # search for valid target above mid
+  p binary_search_iterative(7, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  #=> expect to return index 6
+
+  # search for invalid target below mid
+  p binary_search_iterative(2, [1, 3, 4, 5, 6, 7, 8, 9, 10])
+  #=> expect to return false
+
+  # search for invalid target above mid
+  p binary_search_iterative(7, [1, 2, 3, 4, 5, 6, 8, 9, 10])
+  #=> expect to return false
+
+  # search for target outside lower bound
   p binary_search_iterative(0, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
   #=> expect to return false
+
+  # search for target outside upper bound
   p binary_search_iterative(11, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
   #=> expect to return false
 
